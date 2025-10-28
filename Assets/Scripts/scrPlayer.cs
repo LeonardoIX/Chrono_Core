a149ca970ad2a5f72566a01996fec718ea96b418
Leo <leonardo.otaviano@sou.unifal-mg.edu.br>
2025-10-28 19:17:04 -0300

    Adding player movement

--- Assets/Scripts/scrPlayer.cs
+++ Assets/Scripts/scrPlayer.cs
@@ -1,16 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class scrPlayer : MonoBehaviour
 {
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rig;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
     }
+
+    // 💡 Correction: Added parentheses () to make it a proper method.
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
 }
