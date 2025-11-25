4743b640f94da259313ce9e3b82921eb22312a4e
Enzo <enzomaranezi3@outlook.com>
2025-11-25 14:19:50 -0300

    Transição de cena com animação

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
+
+    private void Awake(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    //Script para trocar de cena "level"
+    public void NextLevel(){
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);  
+        transitionAnim.SetTrigger("Start");
+    }
+}
